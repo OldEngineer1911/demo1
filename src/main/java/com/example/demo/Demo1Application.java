39feb867ec1fc7bae0b176de5ee5d9a3c9ecdf66
Old Engineer <oldengineer1911@gmail.com>
2022-01-02 15:17:11 +0100

    Apply fix based on https://stackoverflow.com/a/70552646/10821695

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import java.util.List;
 
@@ -9,12 +10,12 @@ import java.util.List;
 public class Demo1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
-        Products products = new Products();
+        ApplicationContext app = SpringApplication.run(Demo1Application.class, args);
+        Products products = app.getBean(Products.class);
         List<String> productsFromApplicationYml = products.getProducts();
 
         System.out.println(productsFromApplicationYml.size()); // I would like to see 2
-        products.getProducts().forEach(System.out::println); // I would like to see "first" and "second"
+        productsFromApplicationYml.forEach(System.out::println); // I would like to see "first" and "second"
     }
 
 }
