de360d22436934e992ed442e1df704edae9ba75c
Old Engineer <oldengineer1911@gmail.com>
2022-01-06 21:17:32 +0100

    Aply working fix

--- src/main/java/com/example/demo/Products.java
+++ src/main/java/com/example/demo/Products.java
@@ -3,15 +3,18 @@ package com.example.demo;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @ConfigurationProperties(prefix="products")
 public class Products {
-    private List<String> products = new ArrayList<>();
+    private List<String> products;
 
     public List<String> getProducts() {
         return products;
+    }
+
+    public void setProducts(List<String> products) {
+        this.products = products;
     }
 }
